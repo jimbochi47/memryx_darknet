b1104aa42571d5a2a63018251e65d3b494f99a74
jimbochi47 <jimbochi@memryx.com>
2022-03-24 15:27:35 +0800

    [Feature] Add set_weights feature for conv
    Read weights file and replace conv weights value with file weights.
    weights file order: biases, weightswq

--- examples/mix_ver.c
+++ examples/mix_ver.c
@@ -4,10 +4,82 @@
 #include <sys/time.h>
 #include <assert.h>
 
+char* l_type[] = {
+    "CONVOLUTIONAL",
+    "DECONVOLUTIONAL",
+    "CONNECTED",
+    "MAXPOOL",
+    "SOFTMAX",
+    "DETECTION",
+    "DROPOUT",
+    "CROP",
+    "ROUTE",
+    "COST",
+    "NORMALIZATION",
+    "AVGPOOL",
+    "LOCAL",
+    "SHORTCUT",
+    "ACTIVE",
+    "RNN",
+    "GRU",
+    "LSTM",
+    "CRNN",
+    "BATCHNORM",
+    "NETWORK",
+    "XNOR",
+    "REGION",
+    "YOLO",
+    "ISEG",
+    "REORG",
+    "UPSAMPLE",
+    "LOGXENT",
+    "L2NORM",
+    "BLANK"
+};
+
+void set_weights(network* net, char* weightfile) {
+
+    printf("Loading weigts files: %s \n", weightfile);
+    FILE* input_file = fopen(weightfile, "r");
+    if (!input_file) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < net->n; ++i){
+        layer l = net->layers[i];
+        printf("layer type is %s\n", l_type[l.type]);
+        if(l.type == CONVOLUTIONAL) {
+            int num = l.c/l.groups*l.n*l.size*l.size;
+            float* weight_array = calloc((num+l.n), sizeof(float));
+            char line_buffer[20];
+            int i = 0;
+            while(fgets(line_buffer, sizeof(line_buffer), input_file) != NULL) {
+                if(i >= num)
+                    break;
+                line_buffer[strcspn(line_buffer, "\n")] = 0;
+                // printf("jimbo inside: %s \n", line_buffer);
+                weight_array[i] = atof(line_buffer);
+                i++;
+            }
+
+            for(i = 0; i < l.n; i++) {
+                l.biases[i] = weight_array[i];
+                printf("convolutional biases: %f \n", l.biases[i]);
+            }
+            for(i = 0; i < num; i++) {
+                l.weights[i] = weight_array[i+l.n];
+                printf("convolutional weights: %f \n", l.weights[i]);
+            }
+        }
+    }
+}
+
 void run_mixver(char *cfgfile, char *weightfile, char* filename)
 {
     network *net = load_network(cfgfile, weightfile, 0);
     set_batch_network(net, 1);
+    set_weights(net, weightfile);
 
     image img_data_in;
     if(filename) {
